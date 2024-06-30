72faa449d87fdb25bc02694b581b7a5de1f97f86
Bryan Porter <bryan@bryanporter.com>
2024-06-30 16:38:33 +0000

    woo sql queries in a devcontainer written from an ipad

--- gatherist.web/Program.cs
+++ gatherist.web/Program.cs
@@ -25,12 +25,39 @@ todosApi.MapGet("/{id}", (int id) =>
         ? Results.Ok(todo)
         : Results.NotFound());
 
+var tenantApi = app.MapGroup("/tenants");
+tenantApi.MapGet("/", async () => {
+    using var connection = new Microsoft.Data.SqlClient.SqlConnection();
+    connection.ConnectionString = "Data Source=localhost,1433;Initial Catalog=gatherist.db;User Id=sa;Password=P@ssw0rd;TrustServerCertificate=true";
+
+    await connection.OpenAsync();
+
+    var cmd = connection.CreateCommand();
+    cmd.CommandType = System.Data.CommandType.Text;
+    cmd.CommandText = "SELECT * FROM [dbo].[tenant]";
+
+    using var reader = await cmd.ExecuteReaderAsync();
+
+    var results = new List<Tenant>();
+
+    while (await reader.ReadAsync())
+    {
+        results.Add(
+            new Tenant(reader.GetInt32(0), reader.GetGuid(1), reader.GetString(2), reader.GetDateTime(3), reader.GetDateTime(4), reader.GetBoolean(5))
+        );
+    }
+
+    return results;
+});
+
 // Set up an API endpoint that queries the database running on localhost and exposes CRUD metods
 // for the table tenant in the userdata schema. The query should used Dapper and be compatible with
 // AOT.
 
 
 app.Run();
+
+public record Tenant(int Id, Guid Key, string Name, DateTime CreatedOn, DateTime UpdateAt, bool IsEnabled);
 
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
